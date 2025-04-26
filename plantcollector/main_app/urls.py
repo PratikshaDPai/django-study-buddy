490ea3a23be8ffeed67b0030440816d796fa4beb
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-26 15:25:10 -0700

    Add home view

--- plantcollector/main_app/urls.py
+++ plantcollector/main_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Import views to connect routes to view functions
 
 urlpatterns = [
-    # Routes will be added here
+    path("", views.home, name="home"),
 ]
