3122ee472983165cc5ee707675b6eb4a3b11c3d5
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-27 23:35:14 -0700

    Add create plant page

--- plantcollector/main_app/views.py
+++ plantcollector/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 
 from .models import Plant
 
@@ -24,3 +25,9 @@ def about(request):
 def plant_detail(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
     return render(request, "plants/details.html", {"plant": plant})
+
+
+class PlantCreate(CreateView):
+    model = Plant
+    fields = "__all__"
+    success_url = "/plants/"
