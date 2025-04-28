6051210020144357029593723b7dc655f95777ef
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-28 00:51:10 -0700

    Add User to plants

--- plantcollector/main_app/models.py
+++ plantcollector/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Plant(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # new code below
     def __str__(self):
