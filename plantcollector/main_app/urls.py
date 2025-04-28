daeb8df86e12a2eb09d32f77fceca5041a2e3bf3
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-28 00:39:52 -0700

    Add url for watering and make form action

--- plantcollector/main_app/urls.py
+++ plantcollector/main_app/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("plants/create/", views.PlantCreate.as_view(), name="plant-create"),
     path("plants/<int:pk>/update/", views.PlantUpdate.as_view(), name="plant-update"),
     path("plants/<int:pk>/delete/", views.PlantDelete.as_view(), name="plant-delete"),
+    path(
+        "plants/<int:plant_id>/add-watering/", views.add_watering, name="add-watering"
+    ),
 ]
--- plantcollector/main_app/views.py
+++ plantcollector/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -29,6 +29,19 @@ def plant_detail(request, plant_id):
     return render(
         request, "plants/details.html", {"plant": plant, "watering_form": watering_form}
     )
+
+
+def add_watering(request, plant_id):
+    # create a ModelForm instance using the data in request.POST
+    form = WateringForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the plant_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.plant_id = plant_id
+        new_feeding.save()
+    return redirect("plant-detail", plant_id=plant_id)
 
 
 class PlantCreate(CreateView):
