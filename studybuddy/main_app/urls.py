5c925e937f64f662d252b5fc9de86aacb2b053d9
ppai <1312pratiksha@gmail.com>
2025-05-06 23:47:09 -0700

    Change route to groups

--- studybuddy/main_app/urls.py
+++ studybuddy/main_app/urls.py
@@ -3,16 +3,16 @@ from . import views  # Import views to connect routes to view functions
 
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
-    path("plants/", views.plant_index, name="plant-index"),
+    path("groups/", views.plant_index, name="plant-index"),
     path("about/", views.about, name="about"),
-    path("plants/<int:plant_id>/", views.plant_detail, name="plant-detail"),
-    path("plants/create/", views.StudyGroupCreate.as_view(), name="plant-create"),
+    path("groups/<int:plant_id>/", views.plant_detail, name="plant-detail"),
+    path("groups/create/", views.StudyGroupCreate.as_view(), name="plant-create"),
     path(
-        "plants/<int:pk>/update/", views.StudyGroupUpdate.as_view(), name="plant-update"
+        "groups/<int:pk>/update/", views.StudyGroupUpdate.as_view(), name="plant-update"
     ),
     path(
-        "plants/<int:pk>/delete/", views.StudyGroupDelete.as_view(), name="plant-delete"
+        "groups/<int:pk>/delete/", views.StudyGroupDelete.as_view(), name="plant-delete"
     ),
-    path("plants/<int:plant_id>/add-message/", views.add_message, name="add-message"),
+    path("groups/<int:plant_id>/add-message/", views.add_message, name="add-message"),
     path("accounts/signup/", views.signup, name="signup"),
 ]
